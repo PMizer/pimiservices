87d6d48c7bddb397c66cb111426da880551dd039
pmizer <mizer.piotr@gmail.com>
2023-12-03 21:01:39 +0100

    Adding another microservice that checks if the user is fraudulent.

--- customer/src/main/java/com/pimi/customer/CustomerService.java
+++ customer/src/main/java/com/pimi/customer/CustomerService.java
@@ -1,15 +1,30 @@
 package com.pimi.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest){
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
                 .lastName(customerRegistrationRequest.lastName())
                 .email(customerRegistrationRequest.email())
                 .build();
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        FrauchCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FrauchCheckResponse.class,
+                customer.getId()
+        );
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
